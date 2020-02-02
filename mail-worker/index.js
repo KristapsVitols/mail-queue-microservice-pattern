00b46e80e8e32e57b18675929fcc34e84463acfc
kristapsvitols <kristapsvitols91@gmail.com>
2020-02-02 12:28:32 +0200

    add mailer

--- mail-worker/index.js
+++ mail-worker/index.js
@@ -1,14 +1,38 @@
 const redis = require('redis');
 const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
 
 dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
+    auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASSWORD,
+    },
+});
+
 
 const redisSubscriber = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 redisSubscriber.on('message', (channel, dataString) => {
     const data = JSON.parse(dataString);
 
-    console.log(data);
+    console.log('Sending mail...');
+
+    transporter.sendMail({
+        from: '"Test" <test@example.com>',
+        to: 'test1@example.com',
+        subject: data.title,
+        text: data.message,
+    }, (err, info) => {
+        if (err) {
+            return console.error(`Shit went bad - ${err}`)
+        }
+
+        console.log(`Message sent: ${JSON.stringify(info)}`);
+    });
 });
 
 redisSubscriber.subscribe('send');
